4ac9020d80fd65128663cdd9ba23db5fb1244d9e
Ben Sinclair <ben.sinclair@leith.co.uk>
2022-04-28 15:23:53 +0100

    Add resources link

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -1,4 +1,7 @@
 import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import Stack from '@mui/material/Stack';
 import ChatIcon from '@mui/icons-material/Chat';
 import Link from '@mui/material/Link';
 import Toolbar from '@mui/material/Toolbar';
@@ -13,14 +16,32 @@ const Navigation = (props) => {
 
   return (
     <AppBar position="relative">
-      <Toolbar>
-        <Link href="/">
-          <ChatIcon sx={{ mr: 2 }} />
-        </Link>
+      <Toolbar sx={{display: "flex", justifyContent: "space-between"}}>
+        <Stack direction="row" alignItems="center">
+          <Link href="/">
+            <ChatIcon sx={{ mr: 2 }} />
+          </Link>
 
-        <Typography variant="h1" sx={{ fontSize: "1rem" }} color="inherit" noWrap>
-          {title}
-        </Typography>
+          <Typography variant="h1" sx={{ fontSize: "1rem" }} color="inherit" noWrap>
+            {title}
+          </Typography>
+        </Stack>
+
+        <Stack
+          direction="row"
+          spacing={2}
+          divider={<Divider orientation="vertical" flexItem />}
+        >
+    {/*
+          <Link href="/codebases">
+            Codebases
+          </Link>
+          */}
+
+          <Link href="/resources">
+            Resources
+          </Link>
+        </Stack>
       </Toolbar>
     </AppBar>
   );
